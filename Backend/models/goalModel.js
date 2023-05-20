44735ba02f9815ffa3e92f55fad903afac6ab1d8
nsudekenechi <nsudekenechi2@gmail.com>
2023-05-20 04:43:54 +0100

    Created Schema For User

--- Backend/models/goalModel.js
+++ Backend/models/goalModel.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 const goalModel = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
     text: {
         type: String,
         required: [true, "Text Should be included"]
---
+++ Backend/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter Your Name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter Your Email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter Your Password"]
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", userSchema);
