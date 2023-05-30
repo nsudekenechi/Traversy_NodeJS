a83fd4a29f82cce732b69175a7baef34882d4f6f
nsudekenechi <nsudekenechi2@gmail.com>
2023-05-30 11:04:49 +0100

    Finished Login

--- Frontend/Pages/Login.jsx
+++ Frontend/Pages/Login.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <section>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Login Your Account</p>
+      </section>
+      <form action="">
+        <div>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Enter Your Email"
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter Your Password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="btn">
+          <button onSubmit={onSubmit}>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
